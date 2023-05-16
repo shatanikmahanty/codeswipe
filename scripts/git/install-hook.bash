0d50be414125fc32be141edb5b93c86f58bd22c5
Shatanik Mahanty <shatanikmahanty@gmail.com>
2023-05-16 22:51:21 +0530

    Updated Git hook

--- scripts/git/install-hook.bash
+++ scripts/git/install-hook.bash
@@ -4,5 +4,5 @@ GIT_DIR=$(git rev-parse --git-dir)
  
 echo "Installing hooks..."
 # this command creates copy our pre-commit script in .git/hooks folder
-cp ./scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
+cp ./scripts/git/pre-commit.bash $GIT_DIR/hooks/pre-commit
 echo "Done!"
